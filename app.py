cf635f418552621868a38ddd7187d93da4c6bdfc
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-30 14:14:29 -0500

    user model and app skeleton

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +14,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+# TODO: where does this go?
+db.create_all()
+
+@app.get
--- models.py
+++ models.py
@@ -1,1 +1,25 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    """User database model"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,)
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False,)
+
+    image_url = db.Column(
+        db.Text,
+        nullable=True,)
+
